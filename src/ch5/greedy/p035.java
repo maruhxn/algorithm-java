eebcb254e80efa3931397dad10ee8cfb65c0d1de
maruhxn <maruhan1016@gmail.com>
2024-06-13 11:28:29 +0900

    day11 - greedy

---
+++ src/ch5/greedy/p035.java
@@ -0,0 +1,50 @@
+package ch5.greedy;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 회의실 배정(S1)
+// 시작 시간이 빠른 순?
+// 종료 시간이 빠른 순? -> ok
+// 회의 시간이 짧은 순?
+public class p035 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<TimeInfo> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            arr.add(new TimeInfo(start, end));
+        }
+
+        arr.sort((t1, t2) -> {
+            if (t1.end != t2.end) {
+                return t1.end > t2.end ? 1 : -1;
+            } else {
+                return t1.start > t2.start ? 1 : -1;
+            }
+        });
+
+        int result = 0;
+        TimeInfo prevTi = new TimeInfo(0, 0);
+        for (int i = 0; i < n; i++) {
+            TimeInfo ti = arr.get(i);
+            if (ti.start >= prevTi.end) {
+                prevTi = ti;
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    static class TimeInfo {
+        int start;
+        int end;
+
+        public TimeInfo(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
---
+++ src/ch5/greedy/p036.java
@@ -0,0 +1,41 @@
+package ch5.greedy;
+
+// 최솟값을 만드는 괄호 배치 찾기(S2)
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * + 또는 -가 나오기 전까지의 문자를 parseInt
+ * -가 나오면 다음 -가 나올 때까지 나오는 모든 수 더하기.
+ */
+public class p036 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int result = 0;
+        if (str.contains("-")) {
+            String[] sub = str.split("\\-");
+            for (int i = 0; i < sub.length; i++) {
+                if (sub[i].contains("+")) {
+                    String[] numbers = sub[i].split("\\+");
+                    int sum = 0;
+                    for (String number : numbers) {
+                        sum += Integer.parseInt(number);
+                        sub[i] = String.valueOf(sum);
+                    }
+                }
+                if (i == 0) result += Integer.parseInt(sub[i]);
+                else result -= Integer.parseInt(sub[i]);
+            }
+        } else {
+            String[] numbers = str.split("\\+");
+            for (String number : numbers) {
+                result += Integer.parseInt(number);
+            }
+        }
+        System.out.println(result);
+    }
+}
