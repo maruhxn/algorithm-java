ca5196f7d23f31e919ef740d84bf1a1182a0e3af
maruhxn <maruhan1016@gmail.com>
2025-08-06 13:01:44 +0900

    8/6

---
+++ src/boj/priorityqueue/BOJ2075.java
@@ -0,0 +1,28 @@
+package boj.priorityqueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                pq.offer(num);
+                if (pq.size() > N) pq.poll();
+            }
+        }
+
+        System.out.println(pq.peek());
+    }
+}
